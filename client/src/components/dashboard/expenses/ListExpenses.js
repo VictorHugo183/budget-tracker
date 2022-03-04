8be53689301639a7754a7d8ed0fb94b887d3a702
VICTOR HUGO Nascimento <victorhugovaranda@hotmail.com>
2022-03-04 18:43:24 +0000

    integrated editExpense into listExpenses

--- client/src/components/dashboard/expenses/ListExpenses.js
+++ client/src/components/dashboard/expenses/ListExpenses.js
@@ -2,10 +2,40 @@ import React, { useEffect, useState } from "react";
 import EditExpense from "./EditExpense";
 import { FaTimesCircle } from 'react-icons/fa';
 
+import { FaEdit } from 'react-icons/fa';
+
 
 const ListExpenses = ({ allExpenses, setExpensesChange }) => {
 
   const [expenses, setExpenses] = useState([]);
+
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+  const [target, setTarget] = useState("");
+
+  /* update */
+
+  const updateDescription = async (e) => {
+    e.preventDefault();
+    try {
+      const bodyDesc = { description, amount, category }
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("token", localStorage.token);
+      const response = await fetch(`/dashboard/expenses/${target}`, {
+        method: "PUT",
+        headers: myHeaders,
+        body: JSON.stringify(bodyDesc)
+      })
+
+      /* passed on from dashboard to ListExpenses down to EditExpense, changing this will trigger the useEffect in Dashboard and will update user info without refreshing */
+      setExpensesChange(true);
+
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
 
   /* delete todo function */
   const deleteExpense = async (id) => {
@@ -31,7 +61,7 @@ const ListExpenses = ({ allExpenses, setExpensesChange }) => {
     <>
       <h2>Expenses:</h2>
       {/* table-scroll-y cystom-scroll */}
-      <div className="table-responsive">
+      <div className="table-responsive table-scroll-y custom-scroll">
         {(expenses.length === 0 || expenses[0].expense_id === null) &&
         <p className="empty-table-header">Add a transaction to begin tracking your finances</p>
         }
@@ -52,13 +82,48 @@ const ListExpenses = ({ allExpenses, setExpensesChange }) => {
                 <td>{item.description}</td>
                 <td>{item.amount}</td>
                 <td>{item.category}</td>
-                <td><EditExpense expense={item} setExpensesChange={setExpensesChange}/></td>
+                <td>{/* <EditExpense expense={item} setExpensesChange={setExpensesChange}/> */}
+                  <FaEdit role="button" className="" color="#f5f5f5" size="1.5rem" data-bs-toggle="modal" data-bs-target={`#id${target}`} onClick={() => { setTarget(item.expense_id); setDescription(item.description); setAmount(item.amount); setCategory(item.category);}} /></td>
                 <td><FaTimesCircle role="button" className="mt-1" size="1.5rem" color="#f94144" onClick={() => deleteExpense(item.expense_id)} /></td>
               </tr>
             ))) 
             }
           </tbody>
         </table>
+      </div>
+
+      <div className="modal" id={`id${target}`} style={{ color: "#f5f5f5" }}>
+        <div className="modal-dialog">
+          <div className="modal-content" style={{ backgroundColor: "#31435b" }}>
+
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Expense</h4>
+              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" onClick={() => { setDescription(description); setAmount(amount); setCategory(category) }}></button>
+            </div>
+
+            <div className="modal-body">
+              <label>Description</label>
+              <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)} />
+              <label>Amount</label>
+              <input type="number" min="0" step="0.01" className="form-control" required value={amount} onChange={e => setAmount(e.target.value)} />
+              <label>Category</label>
+              <select className="form-control" value={category} onChange={e => setCategory(e.target.value)}>
+                <option value="Housing">Housing</option>
+                <option value="Food">Food</option>
+                <option value="Transport">Transport</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Personal Care">Personal Care</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+
+            <div className="modal-footer">
+              <button type="button" className="btn btn-login" data-bs-dismiss="modal" onClick={e => updateDescription(e)}>Apply</button>
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => { setDescription(description); setAmount(amount); setCategory(category) }}>Cancel</button>
+            </div>
+
+          </div>
+        </div>
       </div>
     </>
   );
