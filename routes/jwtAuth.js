68efc343065a31b98756d2e9462db5ee124ce00a
VICTOR HUGO Nascimento <victorhugovaranda@hotmail.com>
2022-03-04 16:41:09 +0000

    heroku

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -73,7 +73,7 @@ router.post("/verify", authorization, async (req,res) => {
     res.json(true);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Server Error")
+    res.status(500).send("Unauthorized")
   }
 })
 
